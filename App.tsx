02fc4bafc248476c55219a259fe4f76a74230a6d
Ka9paa <caydenperson0@gmail.com>
2025-12-07 03:38:40 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -103,7 +103,7 @@ export default function App() {
         onNavigate={handleNavigate}
         isLoggedIn={isLoggedIn}
       />
-      <main className="w-full pt-16">{renderPage()}</main>
+      <main className="w-full">{renderPage()}</main>
     </div>
   );
 }
